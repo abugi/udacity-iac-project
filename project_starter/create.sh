bb5ef79cc4cdc29ca32e605938da0fd8ca21a0cc
abugi <abugiboi@gmail.com>
2023-01-06 10:31:02 +0100

    initial commit

---
+++ project_starter/create.sh
@@ -0,0 +1,1 @@
+aws cloudformation create-stack --stack-name $1 --template-body file://project_starter/$2  --parameters file://project_starter/$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
